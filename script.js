267ab0d535cff4249b838b37730463b94a2df1b5
Yigyu <145227350+Y-igyu@users.noreply.github.com>
2024-01-25 16:40:37 -0600

    Made the grid size able to change
    The grid can now be up 100 by 100 or 2 by 2. The container style was removed and added to the wrapper class that is added to remove and clear previous grid after changing its size with the 'skayka' reset button.

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
+const resetButton = document.querySelector('#reset-button');
 
+const createGrid = (size) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    wrapper.setAttribute("style", "border: solid; width: 960px; height: 960px; display: flex;")
+    for (let i = 1; i <= size; i++) {
+        let column = document.createElement('div');
+        column.classList.add('column');
 
-for (let i = 1; i <= 16; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
-    container.appendChild(column);
-    for (let j = 1; j <= 16; j++) {
-        let row = document.createElement("div");
-        row.classList.add("row", "tile");
-        row.addEventListener("mouseover", paint);
-        column.appendChild(row);
-    }
+        for (let j = 1; j <= size; j++) {
+            let row = document.createElement('div');
+            row.classList.add('row', 'tile');
+            row.addEventListener('mouseenter', () => {row.style.backgroundColor = 'black'});
+            column.appendChild(row);
+        };
+
+        wrapper.appendChild(column);
+    };
+    container.appendChild(wrapper);
 };
 
-function paint() {
-    this.style.backgroundColor = "black";
-};
+createGrid(16);
+
+
+resetButton.addEventListener('click', () => {
+    let userSize = Number(prompt("What size you want cuh?"))
+    
+    while (userSize > 100 || userSize < 2) {
+        userSize = Number(prompt("Yo cuh min is 2 and max is 100. Don't make my job harder."));
+    };
+
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.remove();
+    createGrid(userSize);
+});
