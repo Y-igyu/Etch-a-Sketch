9df845dd53e011b7dd3a11d9a169d4195f8fce8f
Yigyu <145227350+Y-igyu@users.noreply.github.com>
2024-01-25 13:33:30 -0600

    Hovering over tile in grid will make it black

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ for (let i = 1; i <= 16; i++) {
     container.appendChild(column);
     for (let j = 1; j <= 16; j++) {
         let row = document.createElement("div");
-        row.classList.add("row");
+        row.classList.add("row", "tile");
+        row.addEventListener("mouseover", paint);
         column.appendChild(row);
     }
-}
+};
+
+function paint() {
+    this.style.backgroundColor = "black";
+};
